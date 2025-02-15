e835ca6507d1314bf2efac55e48fe57463a4d0e5
tac1DEV <sansberrot@gmail.com>
2025-02-15 19:32:05 +0100

    modif du topbar admin

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ include 'functions/main-functions.php';
     <li><a href="index.php?page=dashboard">Accueil</a></li>
     <li><a href="index.php?page=create">Create</a></li>
     <li><a href="index.php?page=getAll">GetAll</a></li>
-    <li><a href="../index.php?page=home">Blog</a></li>
+    <li><a href="index.php?page=blog">Blog</a></li>
     <li><a href="index.php?page=logout">Déconnexion</a></li>
     </ul>
 </nav>
